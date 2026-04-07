0aaa0c649b8206015742431d9409cbe71a4c8a8b
The Judge <53906078+thejudge156@users.noreply.github.com>
2026-04-06 20:49:42 -0700

    Oopsie

--- src/main/java/me/judge/mcnativebuilder/processors/AndroidProcessor.java
+++ src/main/java/me/judge/mcnativebuilder/processors/AndroidProcessor.java
@@ -3,6 +3,7 @@ package me.judge.mcnativebuilder.processors;
 import me.judge.mcnativebuilder.Main;
 import org.angelauramc.judgelib.installer.JudgeLibInstall;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -25,7 +26,8 @@ public class AndroidProcessor implements IProcessor {
         if(!capCacheFolder.exists()) {
             capCacheFolder.mkdirs();
             try {
-                Main.startCompile(classpath, Main.buildDir, "Ob", "serial",
+
+                Process process = Main.startCompile(classpath, Main.buildDir, "Ob", "serial",
                         "-H:CAPCacheDir=" + capCacheFolder,
                         "-H:-UseContainerSupport",
                         "-H:-UseCAPCache",
@@ -37,7 +39,14 @@ public class AndroidProcessor implements IProcessor {
                         "--native-compiler-options=-D__aarch64__",
                         "--native-compiler-options=-I" + System.getenv("ANDROID_NDK") + "/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
                 );
-            } catch (IOException e) {
+                BufferedReader errors = process.errorReader();
+                BufferedReader info = process.inputReader();
+                while (process.isAlive()) {
+                    Thread.sleep(100);
+                }
+                errors.close();
+                info.close();
+            } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
